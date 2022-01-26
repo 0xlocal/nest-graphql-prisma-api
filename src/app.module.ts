5cdc4c7169c0e25ecf45e9ba69614aa9622af251
Ferrel John Fernando <ferreluchiha@gmail.com>
2022-01-26 08:17:16 +0700

    removing unnecessary things

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
 
 @Module({
@@ -12,7 +10,7 @@ import { PostsModule } from './posts/posts.module';
     }),
     PostsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
